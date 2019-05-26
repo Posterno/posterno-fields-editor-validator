3a05a589030db66cdb44122c897df8184247a26b
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-05-26 12:03:37 +0100

    hello world

---
+++ scripts-loader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Scripts registration for the forms editor.
+ *
+ * @package     posterno
+ * @copyright   Copyright (c) 2019, Sematico, LTD
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
