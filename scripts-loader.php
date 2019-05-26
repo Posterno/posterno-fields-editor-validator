e76fe5db97f33e39b538a4f11d8beec0cd509e9d
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-05-26 17:46:24 +0100

    add admin notice for default fields

--- scripts-loader.php
+++ scripts-loader.php
@@ -42,8 +42,6 @@ function pno_get_listing_custom_fields_page_vars() {
 
 	global $post;
 
-	$existing_vars = pno_get_users_custom_fields_page_vars();
-
 	$js_vars = [
 		'field_id'         => carbon_get_post_meta( $post->ID, 'listing_field_meta_key' ),
 		'field_type'       => carbon_get_post_meta( $post->ID, 'listing_field_type' ),
@@ -59,21 +57,45 @@ function pno_get_listing_custom_fields_page_vars() {
 }
 
 /**
- * Load scripts into the editor.
+ * Tell the user, the field can't be deleted.
  *
  * @return void
  */
-function pno_fields_editor_scripts() {
+function pno_is_default_field_notice() {
 
-	$screen  = get_current_screen();
-	$version = PNO_VERSION;
+	$screen = get_current_screen();
 
-	wp_register_script( 'posterno-listings-fields-validator', PNO_PLUGIN_URL . 'vendor/posterno/posterno-fields-editor-validator/dist/js/listings.js', [], $version, true );
+	global $post;
 
-	if ( $screen->id === 'pno_listings_fields' ) {
-		wp_enqueue_script( 'posterno-listings-fields-validator' );
-		wp_localize_script( 'posterno-listings-fields-validator', 'pno_listings_cf', pno_get_custom_fields_editor_js_vars() );
+	$message = esc_html__( 'This is a default field. Default fields cannot have their type and meta key changed and can\'t be deleted.' );
+
+	if ( $screen instanceof WP_Screen && $screen->id === 'pno_users_fields' ) {
+
+		if ( $post instanceof WP_Post && isset( $post->ID ) && ! posterno()->admin_notices->is_dismissed( 'field_is_default_' . $post->ID ) ) {
+			$key = carbon_get_post_meta( $post->ID, 'profile_field_meta_key' );
+			if ( pno_is_default_field( $key ) ) {
+				posterno()->admin_notices->register_notice( 'field_is_default_' . $post->ID, 'info', $message, [ 'dismissible' => false ] );
+			}
+		}
+	} elseif ( $screen instanceof WP_Screen && $screen->id === 'pno_signup_fields' ) {
+
+		if ( $post instanceof WP_Post && isset( $post->ID ) && ! posterno()->admin_notices->is_dismissed( 'field_is_default_' . $post->ID ) ) {
+
+			$field       = new \PNO\Database\Queries\Registration_Fields();
+			$found_field = $field->get_item_by( 'post_id', $post->ID );
+
+			if ( ! $found_field->canDelete() ) {
+				posterno()->admin_notices->register_notice( 'field_is_default_' . $post->ID, 'info', $message, [ 'dismissible' => false ] );
+			}
+		}
+	} elseif ( $screen instanceof WP_Screen && $screen->id === 'pno_listings_fields' ) {
+
+		if ( $post instanceof WP_Post && isset( $post->ID ) && ! posterno()->admin_notices->is_dismissed( 'field_is_default_' . $post->ID ) ) {
+			$key = carbon_get_post_meta( $post->ID, 'listing_field_meta_key' );
+			if ( pno_is_default_field( $key ) ) {
+				posterno()->admin_notices->register_notice( 'field_is_default_' . $post->ID, 'info', $message, [ 'dismissible' => false ] );
+			}
+		}
 	}
-
 }
-add_action( 'admin_enqueue_scripts', 'pno_fields_editor_scripts' );
+add_action( 'admin_head', 'pno_is_default_field_notice' );
